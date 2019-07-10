91334ce6d954477dbab854bc7f6e7ec1714c7228
Adrian SR <52725847+Adrians22@users.noreply.github.com>
2019-07-09 21:24:59 -0500

    Update Ing_sw_2019.java

--- ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
+++ ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
@@ -16,7 +16,7 @@ public class Ing_sw_2019 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        printf("hola");
+        println("hola");
     }
     
 }
